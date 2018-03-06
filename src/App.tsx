c404ce1c7d4e27c9a20684504dcff01b0d8474b5
kingdaro <kingdaro@gmail.com>
2018-03-05 21:55:19 -0500

    added placeholder home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react"
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 import { AppNav } from "./AppNav"
 import { CharacterEditPage } from "./CharacterEditPage"
 import { CharacterListPage } from "./CharacterListPage"
 import { CharacterPage } from "./CharacterPage"
+import { HomePage } from "./HomePage"
 import { NotFound } from "./NotFound"
 import { routePaths } from "./routePaths"
-
-const redirect = (path: string) => () => <Redirect to={path} />
 
 export class App extends React.Component {
   render() {
@@ -20,7 +19,7 @@ export class App extends React.Component {
           <section className="section">
             <main className="container">
               <Switch>
-                <Route exact path={routePaths.home} render={redirect(routePaths.characterList)} />
+                <Route exact path={routePaths.home} component={HomePage} />
                 <Route exact path={routePaths.characterList} component={CharacterListPage} />
                 <Route exact path={routePaths.viewCharacter(":id")} component={CharacterPage} />
                 <Route exact path={routePaths.editCharacter(":id")} component={CharacterEditPage} />
---
+++ src/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export class HomePage extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1 className="title">nothing here yet</h1>
+        <h2 className="subtitle">lol</h2>
+      </main>
+    )
+  }
+}
