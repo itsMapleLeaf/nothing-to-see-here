bc72bab8730ea8e4dac5895420dd8213de8fd6dd
kingdaro <kingdaro@gmail.com>
2018-03-09 23:13:02 -0500

    added note about FadedText and child elements

--- client/src/styles/text.ts
+++ client/src/styles/text.ts
@@ -3,6 +3,7 @@ import styled from "styled-components"
 export const FadedText = styled.span`
   opacity: 0.5;
 
+  /* HACK: for some reason, spans don't give opacity to child elements */
   > * {
     opacity: 0.5;
   }
