925251a305f2d253f583ebe24fda446b08fc8baa
kingdaro <kingdaro@gmail.com>
2018-03-10 15:44:06 -0500

    NotFound: use RouterLink, remove unnecessary classes

--- client/src/app/routes/NotFound/index.tsx
+++ client/src/app/routes/NotFound/index.tsx
@@ -1,17 +1,15 @@
 import React from "react"
-import { Link } from "react-router-dom"
 
 import { routePaths } from "../../../routePaths"
 import { PageSection, PageTitle, PageWrapper } from "../../../styles/layout"
+import { RouterLink } from "../../../styles/link"
 
 export function NotFound() {
   return (
     <PageWrapper>
-      <PageTitle className="title">oops! unable to find this page</PageTitle>
+      <PageTitle>oops! unable to find this page</PageTitle>
       <PageSection>
-        <Link to={routePaths.home} className="subtitle">
-          Return to Home
-        </Link>
+        <RouterLink to={routePaths.home}>Return to Home</RouterLink>
       </PageSection>
     </PageWrapper>
   )
