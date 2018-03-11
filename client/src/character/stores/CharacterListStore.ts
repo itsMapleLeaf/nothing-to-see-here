03e794b1135819d769e1e57124da747bba6bedf2
kingdaro <kingdaro@gmail.com>
2018-03-11 18:02:02 -0400

    fetch and update the user's own characters

--- client/src/character/stores/CharacterListStore.ts
+++ client/src/character/stores/CharacterListStore.ts
@@ -1,17 +1,33 @@
-import { app } from "firebase/app"
+import { app, firestore, Unsubscribe } from "firebase/app"
 import { action, observable } from "mobx"
 
 export class CharacterListStore {
   @observable characters = [] as any[]
 
   constructor(app: app.App) {
-    app
-      .firestore()
-      .collection("characters")
-      .onSnapshot(snapshot => {
-        const characters = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-        this.setCharacters(characters)
-      })
+    let unsubscribe: undefined | Unsubscribe
+
+    app.auth().onAuthStateChanged(user => {
+      if (unsubscribe) {
+        unsubscribe()
+      }
+
+      if (!user) {
+        this.setCharacters([])
+        return
+      }
+
+      unsubscribe = app
+        .firestore()
+        .doc("users/" + user.uid)
+        .onSnapshot(async userSnapshot => {
+          const userData = userSnapshot.data()
+          const characterRefs = userData.characters as Array<firestore.DocumentReference>
+          const characterSnaps = await Promise.all(characterRefs.map(char => char.get()))
+          const characters = characterSnaps.map(snap => ({ id: snap.id, ...snap.data() }))
+          this.setCharacters(characters)
+        })
+    })
   }
 
   @action
