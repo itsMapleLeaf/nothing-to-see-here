b518bc91c779475378f760942dcde39cd8c9020a
kingdaro <kingdaro@gmail.com>
2018-03-12 18:48:54 -0400

    index: stores -> rootStore

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,12 +10,12 @@ import { StoreProvider } from "./storeContext"
 import { RootStore } from "./stores"
 import { applyGlobalStyles } from "./styles"
 
-function render(stores: any) {
+function render(rootStore: RootStore) {
   const AppComponent = require("./app/components/App").App as typeof App
   applyGlobalStyles()
 
   const root = (
-    <StoreProvider value={stores}>
+    <StoreProvider value={rootStore}>
       <AppComponent />
     </StoreProvider>
   )
@@ -27,11 +27,11 @@ function main() {
   useStrict(true)
 
   const app = initFirebase()
-  const stores = new RootStore(app)
-  render(stores)
+  const rootStore = new RootStore(app)
+  render(rootStore)
 
   if (module.hot) {
-    module.hot.accept("./app/components/App", () => render(stores))
+    module.hot.accept("./app/components/App", () => render(rootStore))
   }
 }
 
