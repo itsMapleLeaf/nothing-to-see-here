bba8ee799b311a920c6a7ce82785755a57a1a53a
kingdaro <kingdaro@gmail.com>
2018-03-12 19:53:38 -0400

    CharacterPage: cleanup, show loading cover if loading takes too long

--- client/src/character/routes/CharacterPage/index.tsx
+++ client/src/character/routes/CharacterPage/index.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { RouteComponentProps } from "react-router"
 
-import { createFetcher } from "../../../common/components/Fetcher"
+import { LoadingCover } from "../../../app/components/LoadingCover"
+import { createFetcher, FetchState } from "../../../common/components/Fetcher"
 import { PageWrapper } from "../../../styles/layout"
 import { fetchCharacterDetailsById } from "../../actions"
+import { CharacterModel } from "../../models/CharacterModel"
 import { CharacterPageDetails } from "./CharacterPageDetails"
 import { CharacterPageError } from "./CharacterPageError"
-import { CharacterModel } from "../../models/CharacterModel";
 
 const CharacterDetailsFetcher = createFetcher<CharacterModel>()
 
@@ -16,17 +17,27 @@ export function CharacterPage(props: RouteComponentProps<{ id: string }>) {
       <CharacterDetailsFetcher
         id={props.match.params.id}
         fetch={fetchCharacterDetailsById}
-        render={state => {
-          switch (state.state) {
-            case "success":
-              return <CharacterPageDetails character={state.data} />
-            case "error":
-              return <CharacterPageError error={state.error} />
-            default:
-              return ""
-          }
-        }}
+        render={state => (
+          <React.Fragment>
+            {renderFetchResult(state)}
+            <LoadingCover
+              message="Fetching character details..."
+              visible={state.state === "fetching-long"}
+            />
+          </React.Fragment>
+        )}
       />
     </PageWrapper>
   )
+
+  function renderFetchResult(state: FetchState<CharacterModel>) {
+    switch (state.state) {
+      case "success":
+        return <CharacterPageDetails character={state.data} />
+      case "error":
+        return <CharacterPageError error={state.error} />
+      default:
+        return ""
+    }
+  }
 }
