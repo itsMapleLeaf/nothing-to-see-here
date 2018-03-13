21c904748be76ad1ac0d46a3959483fba3631679
kingdaro <kingdaro@gmail.com>
2018-03-13 18:18:55 -0400

    CharacterEditForm: use observables

--- client/src/character/components/CharacterEditPage/CharacterEditForm.tsx
+++ client/src/character/components/CharacterEditPage/CharacterEditForm.tsx
@@ -1,3 +1,5 @@
+import { action, observable } from "mobx"
+import { observer } from "mobx-react"
 import * as React from "react"
 import { Redirect } from "react-router-dom"
 
@@ -5,26 +7,32 @@ import { routePaths } from "../../../routePaths"
 import { Button, Input, Label, TextArea } from "../../../styles/elements"
 import { CharacterModel } from "../../models/CharacterModel"
 
+@observer
 export class CharacterEditForm extends React.Component<{ character: CharacterModel }> {
-  state = {
+  @observable
+  fields = {
     name: this.props.character.name,
     tagline: this.props.character.tagline,
-    saved: false,
   }
 
-  changeHandler = (field: string) => (event: React.ChangeEvent<{ value: string }>) => {
-    this.setState({ [field]: event.currentTarget.value })
+  @observable saved = false
+
+  @action
+  handleChange = (event: React.ChangeEvent<{ name: string; value: string }>) => {
+    const { name, value } = event.currentTarget
+    this.fields[name] = value
   }
 
-  handleSubmit = async (event: React.FormEvent<{}>) => {
+  @action
+  handleSubmit = (event: React.FormEvent<{}>) => {
     event.preventDefault()
-    this.setState({ saved: true })
+    this.saved = true
   }
 
   render() {
     const { character } = this.props
 
-    if (this.state.saved) {
+    if (this.saved) {
       return <Redirect push to={routePaths.viewCharacter(character.id)} />
     }
 
@@ -33,20 +41,22 @@ export class CharacterEditForm extends React.Component<{ character: CharacterMod
         <fieldset>
           <Label>Name</Label>
           <Input
+            name="name"
             required
             placeholder="My Awesome Character"
-            value={this.state.name}
-            onChange={this.changeHandler("name")}
+            value={this.fields.name}
+            onChange={this.handleChange}
           />
         </fieldset>
 
         <fieldset>
           <Label>Tagline</Label>
           <TextArea
+            name="tagline"
             height={180}
-            placeholder="Introduce your character here! Who are they? What are they like? What have they done?"
-            value={this.state.tagline}
-            onChange={this.changeHandler("tagline")}
+            placeholder="Enter a short, snappy description of your character."
+            value={this.fields.tagline}
+            onChange={this.handleChange}
           />
         </fieldset>
 
