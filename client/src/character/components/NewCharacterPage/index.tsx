0504e0dd9fd01a072034909f846bf03a97632e6a
kingdaro <kingdaro@gmail.com>
2018-03-15 11:53:45 -0400

    added formik

--- client/src/character/components/NewCharacterPage/index.tsx
+++ client/src/character/components/NewCharacterPage/index.tsx
@@ -1,3 +1,4 @@
+import { Formik, FormikProps } from "formik"
 import { History } from "history"
 import * as React from "react"
 import { Route } from "react-router-dom"
@@ -14,55 +15,73 @@ import {
   PageWrapperPanel,
 } from "../../../styles/elements"
 
+interface FormValues {
+  name: string
+  description: string
+}
+
+class NewCharacterForm extends Formik<{}, FormValues> {}
+
 export const NewCharacterPage = () => {
-  let nameInput: HTMLInputElement
-  let descriptionInput: HTMLInputElement
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, history: History) => {
-    event.preventDefault()
-
-    const { user } = authStore
-    if (!user) return
-
-    await firebaseApp
-      .firestore()
-      .collection("characters")
-      .doc()
-      .set({
-        name: nameInput.value,
-        tagline: descriptionInput.value,
-        owner: user.uid,
-      })
-
-    history.push(routePaths.characterList)
-  }
-
   return (
     <PageWrapperPanel>
       <PageTitle>create new character</PageTitle>
       <PageSection>
         <Route
           render={({ history }) => (
-            <form onSubmit={event => handleSubmit(event, history)}>
-              <fieldset>
-                <Label>Name</Label>
-                <Input name="name" placeholder="awesome-san" innerRef={el => (nameInput = el)} />
-              </fieldset>
-              <fieldset>
-                <Label>Description</Label>
-                <Input
-                  name="description"
-                  placeholder="an awesome character that does awesome things"
-                  innerRef={el => (descriptionInput = el)}
-                />
-              </fieldset>
-              <fieldset>
-                <Button>Submit</Button>
-              </fieldset>
-            </form>
+            <NewCharacterForm
+              initialValues={{ name: "", description: "" }}
+              render={renderForm}
+              onSubmit={createSubmitHandler(history)}
+            />
           )}
         />
       </PageSection>
     </PageWrapperPanel>
   )
+}
+
+const renderForm = (props: FormikProps<FormValues>) => (
+  <form onSubmit={props.handleSubmit}>
+    <fieldset>
+      <Label>Name</Label>
+      <Input
+        name="name"
+        placeholder="awesome-san"
+        value={props.values.name}
+        onChange={props.handleChange}
+      />
+    </fieldset>
+
+    <fieldset>
+      <Label>Description</Label>
+      <Input
+        name="description"
+        placeholder="an awesome character that does awesome things"
+        value={props.values.description}
+        onChange={props.handleChange}
+      />
+    </fieldset>
+
+    <fieldset>
+      <Button>Submit</Button>
+    </fieldset>
+  </form>
+)
+
+const createSubmitHandler = (history: History) => async (values: FormValues) => {
+  const { user } = authStore
+  if (!user) return
+
+  await firebaseApp
+    .firestore()
+    .collection("characters")
+    .doc()
+    .set({
+      name: values.name,
+      tagline: values.description,
+      owner: user.uid,
+    })
+
+  history.push(routePaths.characterList)
 }
