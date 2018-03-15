2500ca0dfcd5f5e00ea08e542cba0403375f3f5a
kingdaro <kingdaro@gmail.com>
2018-03-15 10:58:28 -0400

    index: cleanup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,13 +7,14 @@ import * as ReactDOM from "react-dom"
 import { App } from "./app/components/App"
 import { applyGlobalStyles } from "./styles"
 
+function render() {
+  ReactDOM.render(<App />, document.getElementById("root"))
+}
+
 function main() {
   configure({ enforceActions: true })
   applyGlobalStyles()
-
-  const render = () => {
-    ReactDOM.render(<App />, document.getElementById("root"))
-  }
+  render()
 
   if (module.hot) {
     module.hot.accept("./app/components/App", render)
