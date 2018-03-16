eed22bafe80949a0b0dbe789e1ee89ab8b3034fc
kingdaro <kingdaro@gmail.com>
2018-03-16 01:29:27 -0400

    firebaseActions: DRY it up

--- client/src/character/firebaseActions.ts
+++ client/src/character/firebaseActions.ts
@@ -3,42 +3,27 @@ import { User } from "firebase/app"
 import { firebaseApp } from "../firebase"
 import { CharacterModel } from "./models/CharacterModel"
 
-export async function getCharacterById(id: string) {
-  const doc = await firebaseApp
-    .firestore()
-    .collection("characters")
-    .doc(id)
-    .get()
+const store = firebaseApp.firestore()
+const charactersCollection = store.collection("characters")
 
+export async function getCharacterById(id: string) {
+  const doc = await charactersCollection.doc(id).get()
   return new CharacterModel(doc)
 }
 
 export async function getCharactersOwnedByUser(userId: string) {
-  const result = await firebaseApp
-    .firestore()
-    .collection("characters")
-    .where("owner", "==", userId)
-    .get()
-
+  const result = await charactersCollection.where("owner", "==", userId).get()
   return result.docs.map(doc => new CharacterModel(doc))
 }
 
 export function createCharacter(owner: User, values: { name: string; tagline: string }) {
-  return firebaseApp
-    .firestore()
-    .collection("characters")
-    .doc()
-    .set({
-      name: values.name,
-      tagline: values.tagline,
-      owner: owner.uid,
-    })
+  return charactersCollection.doc().set({
+    name: values.name,
+    tagline: values.tagline,
+    owner: owner.uid,
+  })
 }
 
 export function updateCharacter(id: string, values: Partial<CharacterModel>) {
-  return firebaseApp
-    .firestore()
-    .collection("characters")
-    .doc(id)
-    .update(values)
+  return charactersCollection.doc(id).update(values)
 }
