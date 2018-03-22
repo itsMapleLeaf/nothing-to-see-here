0bfa576e8f1a359457cc576f0d6fe6e1869365e3
kingdaro <kingdaro@gmail.com>
2018-03-22 13:48:12 -0400

    db: change user existence checker
    now it throws with a specific error of which user detail already exists
    
    this might not look like good practice, but doing things this way allows you to know _which_ field already exists, and lets me handle all of the errors back at the top like any other

--- api/src/db.ts
+++ api/src/db.ts
@@ -3,7 +3,7 @@ import securePassword from "secure-password"
 import { promisify } from "util"
 
 import { AccountData } from "./accountData"
-import { databaseUrl, databaseUser, databasePass } from "./env"
+import { databasePass, databaseUrl, databaseUser } from "./env"
 
 const driver = neo4j.driver(databaseUrl, neo4j.auth.basic(databaseUser, databasePass))
 
@@ -22,11 +22,10 @@ const passwordPolicy = securePassword()
 const hashPassword = promisify<Buffer, Buffer>(passwordPolicy.hash.bind(passwordPolicy))
 
 export async function createAccount(accountData: AccountData) {
-  if (await userExists(accountData.username)) {
-    throw Error(`User "${accountData.username}" already exists`)
-  }
+  await verifyUserExistence(accountData)
 
   const hashedPassword = await hashPassword(Buffer.from(accountData.password))
+
   await session.run(`create (:User $accountData)`, {
     accountData: {
       ...accountData,
@@ -35,10 +34,22 @@ export async function createAccount(accountData: AccountData) {
   })
 }
 
-export async function userExists(username: string) {
-  const { records } = await session.run(
-    "match (:User { username: {username} }) return true limit 1",
-    { username },
-  )
-  return records.length > 0
+export async function verifyUserExistence(details: Pick<AccountData, "username" | "email">) {
+  const query = `
+    match (u:User)
+    where u.username = {username} or u.email = {email}
+    return u.username, u.email
+  `
+
+  const result = await session.run(query, details)
+  const record = result.records[0]
+
+  if (record) {
+    if (record.get("u.username") === details.username) {
+      throw Error(`Username "${details.username}" exists`)
+    }
+    if (record.get("u.email") === details.email) {
+      throw Error(`Email "${details.email}" is already registered`)
+    }
+  }
 }
