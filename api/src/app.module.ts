45a604135dbd169b94ad786106becae6e8eb2dc0
kingdaro <kingdaro@gmail.com>
2018-03-28 03:45:33 -0400

    api/app.module: fix import error

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common"
 
 import { AuthController } from "./auth.controller"
-import { AuthService } from "./user.service"
 import { DatabaseService } from "./database.service"
+import { UserService } from "./user.service"
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  components: [DatabaseService, AuthService],
+  components: [DatabaseService, UserService],
 })
 export class AppModule {}
