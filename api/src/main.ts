b286b78c71885e53a0144c03b4dd10a8f7772584
kingdaro <kingdaro@gmail.com>
2018-03-29 17:14:32 -0400

    api: enable CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from "@nestjs/core"
+import cors from "cors"
 
 import { AppModule } from "./app.module"
 import { port } from "./env"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(cors())
+
   await app.listen(port)
   console.info(`listening on http://localhost:${port}`)
 }
