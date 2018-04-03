503241cfa7f23bc664bb7d7e3ace6db6bb4a5406
kingdaro <kingdaro@gmail.com>
2018-04-02 23:17:57 -0400

    client: hook up user auth functionality

--- client/src/AppHeader.tsx
+++ client/src/AppHeader.tsx
@@ -1,10 +1,14 @@
+import { observer } from "mobx-react"
 import * as React from "react"
 import styled from "react-emotion"
 
+import { ClientUserData } from "../../shared/user/types/client-user-data"
 import { preventDefault } from "./helpers/reactHelpers"
 import { modalStore } from "./ModalStore"
 import { buttonFlatStyle } from "./style/button"
 import { foregroundColor, shadowColor } from "./style/colors"
+import { inputPadding } from "./style/input"
+import { userStore } from "./UserStore"
 
 const Header = styled.header`
   background: ${foregroundColor};
@@ -27,18 +31,37 @@ const Nav = styled.nav`
   display: flex;
 `
 
-export function AppHeader() {
+export const AppHeader = observer(() => {
+  const { userData } = userStore
+  const links = userData ? <UserLinks userData={userData} /> : <GuestLinks />
   return (
     <Header>
       <Title>awesome website</Title>
-      <Nav>
-        <a className={buttonFlatStyle} href="#" onClick={preventDefault(modalStore.login.show)}>
-          log in
-        </a>
-        <a className={buttonFlatStyle} href="#" onClick={preventDefault(modalStore.register.show)}>
-          register
-        </a>
-      </Nav>
+      <Nav>{links}</Nav>
     </Header>
+  )
+})
+
+function UserLinks(props: { userData: ClientUserData }) {
+  return (
+    <React.Fragment>
+      <a className={buttonFlatStyle} href="#" onClick={preventDefault(userStore.logout)}>
+        log out
+      </a>
+      <div className={inputPadding}>welcome, {props.userData.displayName}!</div>
+    </React.Fragment>
+  )
+}
+
+function GuestLinks() {
+  return (
+    <React.Fragment>
+      <a className={buttonFlatStyle} href="#" onClick={preventDefault(modalStore.login.show)}>
+        log in
+      </a>
+      <a className={buttonFlatStyle} href="#" onClick={preventDefault(modalStore.register.show)}>
+        register
+      </a>
+    </React.Fragment>
   )
 }
